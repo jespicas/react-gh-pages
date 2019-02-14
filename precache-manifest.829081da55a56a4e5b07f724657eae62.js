8ef3bd60fa05d8274656053d83731c02eef81dda
_espi_ <espi@espipinxo.com>
2019-02-14 20:42:21 +0100

    Updates

---
+++ precache-manifest.829081da55a56a4e5b07f724657eae62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cde22863ffca018955eb",
+    "url": "/react-gh-pages/static/js/runtime~main.cde22863.js"
+  },
+  {
+    "revision": "1168db4bb6ac23782e89",
+    "url": "/react-gh-pages/static/js/main.1168db4b.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/react-gh-pages/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "1168db4bb6ac23782e89",
+    "url": "/react-gh-pages/static/css/main.38b4f9e9.chunk.css"
+  },
+  {
+    "revision": "8439420609e0d4c29ed88093ae8a6f65",
+    "url": "/react-gh-pages/index.html"
+  }
+];
